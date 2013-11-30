19e0ac609d45f17f1ef1451dfde9b8b6f39ea928
xant <xant@xant.net>
2013-11-30 01:03:21 +0100

    unused/unnecessary declaration

--- shardcached/src/storage_mem.h
+++ shardcached/src/storage_mem.h
@@ -1,6 +1,4 @@
 #include <shardcache.h>
 
-extern shardcache_storage_t storage_mem;
-
 shardcache_storage_t *storage_mem_create(const char **options);
 void storage_mem_destroy(shardcache_storage_t *st);
