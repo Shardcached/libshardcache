20f2d1452efcce839043cc1c929ce0461d312923
xant <xant@xant.net>
2013-12-01 03:13:05 +0100

    updated the protocol definition reported in the header as a comment
    was already updated in docs/protocol.txt

--- src/messaging.h
+++ src/messaging.h
@@ -5,29 +5,36 @@
 
 /* protocol specification
  *
- *  GET_MESSAGE      : <AUTH><HDR><KEY>
- *  SET_MESSAGE      : <AUTH><HDR><KEY><VALUE>
- *  DEL_MESSAGE      : <AUTH><HDR><KEY>
- *  EVI_MESSAGE      : <AUTH><HDR><KEY>
- *  RESP_MESSAGE     : <HDR><VALUE>
+ * MESSAGE      : <MSG><SIG>
+ * MSG          : <HDR><RECORD>[<RSEP><RECORD>...]<EOM>
+ * HDR          : <MSG_GET> | <MSG_SET> | <MSG_DEL> | <MSG_EVI> | <MSG_RES>
+ * MSG_GET      : 0x01
+ * MSG_SET      : 0x02
+ * MSG_DEL      : 0x03
+ * MSG_EVI      : 0x04
+ * MSG_RES      : 0x11
+ * RSEP         : 0x80
+ * RECORD       : <SIZE><DATA>[<SIZE><DATA>...]<EOR>
+ * SIZE         : <HIGH_BYTE><LOW_BYTE>
+ * EOR          : <NULL_BYTE><NULL_BYTE>
+ * HIGH_BYTE    : <BYTE>
+ * LOW_BYTE     : <BYTE>
+ * DATA         : <BYTE>...<BYTE>
+ * BYTE         : 0x00 - 0xFF
+ * NULL_BYTE    : 0x00
+ * EOM          : <NULL_BYTE>
+ * SIG          : <BYTE>[8]
+ * KEY          : <RECORD>
+ * VALUE        : <RECORD>
+
+ * The only supported messages are :
+
+ * GET_MESSAGE  : <MSG_GET><KEY><EOM><SIG>
+ * SET_MESSAGE  : <MSG_SET><KEY><RSEP><VALUE><EOM><SIG>
+ * DEL_MESSAGE  : <MSG_DEL><KEY><EOM><SIG>
+ * EVI_MESSAGE  : <MSG_EVI><KEY><EOM><SIG>
+ * RES_MESSAGE  : <MSG_RES><RECORD><EOM><SIG>
  *
- *  AUTH      : <BYTE>[20]
- *  HDR       : MSG_GET | MSG_SET | MSG_DEL | MSG_RESP
- *  MSG_GET   : 0x01
- *  MSG_SET   : 0x02
- *  MSG_DEL   : 0x03
- *  MSG_EVI   : 0x04
- *  MSG_RESP  : 0x11
- *  KEY       : <RECORD>
- *  VALUE     : <RECORD>
- *  RECORD    : <SIZE><DATA>[<SIZE><DATA>...]<EOR>
- *  SIZE      : <HIGH_BYTE><LOW_BYTE>
- *  DATA      : <BYTE>...<BYTE>
- *  EOR       : <NULL_BYTE><NULL_BYTE>
- *  HIGH_BYTE : BYTE
- *  LOW_BYTE  : BYTE
- *  NULL_BYTE : 0x00
- *  BYTE      : 0x00 - 0xFF
  */
 
 // in byte
