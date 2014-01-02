d85c37b87e62ba287ec180cca49142db9707e6db
Andrea Guzzo <andrea.guzzo@booking.com>
2014-01-02 15:44:43 +0100

    examples for the Shardcache::Client::Fast perl module

---
+++ perl/p5-Shardcache-Client-Fast/examples/shc_set.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Shardcache::Client::Fast;
+use File::Slurp;
+use Getopt::Long;
+
+my $USAGE = <<__USAGE ;
+Usage: $0 --key=<key> --input=<input_file> [ --expire=<expire> ] [ --hosts=<hosts> ] [ --secret=<secret> ]
+       $0 --key=<key> --value=<value> [ --expire=<expire> ] [ --hosts=<hosts> ] [ --secret=<secret> ]
+__USAGE
+
+my $infile;
+my $key;
+my $value;
+my $expire;
+my $hosts_param = $ENV{SHC_HOSTS};
+my $secret = $ENV{SHC_SECRET};
+
+
+GetOptions ("input=s"  => \$infile,
+            "key=s"    => \$key,
+            "value=s"  => \$value,
+            "hosts=s"  => \$hosts_param,
+            "expire=s" => \$expire,
+            "secret=s" => \$secret)
+            or die("Error in command line arguments\n");
+
+
+die $USAGE unless ($key && ($value || $infile));
+
+die "SHC_HOSTS environment variable MUST be defined if no --hosts argument is provided\n".
+    $USAGE unless($hosts_param);
+
+my @hosts = split(',', $hosts_param);
+my $c = Shardcache::Client::Fast->new((@hosts > 1) ? \@hosts : $hosts[0], $secret);
+
+if ($infile && !$value) {
+    if ($infile eq '-') {
+        while(<>) {
+            $value .= $_;
+        }
+    } else {
+        $value = read_file($infile);
+    }
+}
+unless ($c->set($key, $value, $expire)) {
+    print "Err\n";
+    exit(-1);
+}
+
+print "Ok\n";
+exit(0);
