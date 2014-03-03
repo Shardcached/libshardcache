689668342d7a0d104709c2e759de5ab79f13f214
Antonio Malara <antonio.malara@booking.com>
2014-03-03 16:13:17 +0100

    creating a simple setup.py

---
+++ python/shardcache-client/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='ShardcacheClient',
+      version='1.0',
+      description='Pure python library for shardcache client',
+      packages=('shardcacheclient',))
+
---
+++ python/shardcache-client/shardcacheclient/__init__.py
@@ -0,0 +1,1 @@
+from shardcacheclient.client import ShardcacheClient
