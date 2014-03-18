204d22cbab46b09ad184f2d26046b834d3a0a931
xant <xant@xant.net>
2014-03-18 23:45:02 +0100

    cleanings + updated libhl because of a fix in the iterators

--- src/shardcache.c
+++ src/shardcache.c
@@ -357,33 +357,35 @@ evictor(void *priv)
     while (!ATOMIC_READ(cache->evictor_quit))
     {
 
-        shardcache_evictor_job_t *job = NULL;
         // this will extract only the first value
-        while (job) {
-            char keystr[1024];
+        shardcache_evictor_job_t *job = NULL;
+        ht_foreach_value(jobs, evict_key, &job);
+        char keystr[1024] = { 0 };
+
+        if (shardcache_log_level() >= LOG_DEBUG)
             KEY2STR(job->key, job->klen, keystr, sizeof(keystr));
-            SHC_DEBUG2("Eviction job for key '%s' started", keystr);
 
-            int i;
-            for (i = 0; i < cache->num_shards; i++) {
-                char *peer = cache->shards[i]->label;
-                if (strcmp(peer, cache->me) != 0) {
-                    SHC_DEBUG3("Sending Eviction command to %s", peer);
-                    int rindex = rand()%cache->shards[i]->num_replicas;
-                    int fd = shardcache_get_connection_for_peer(cache, cache->shards[i]->address[rindex]);
-                    int rc = evict_from_peer(cache->shards[i]->address[rindex], (char *)cache->auth, SHC_HDR_SIGNATURE_SIP, job->key, job->klen, fd);
-                    if (rc != 0)
-                        SHC_WARNING("evict_from_peer return %d for peer %s", rc, peer);
-                    shardcache_release_connection_for_peer(cache, cache->shards[i]->address[rindex], fd);
-                }
+        SHC_DEBUG2("Eviction job for key '%s' started", keystr);
+
+        int i;
+        for (i = 0; i < cache->num_shards; i++) {
+            char *peer = cache->shards[i]->label;
+            if (strcmp(peer, cache->me) != 0) {
+                SHC_DEBUG3("Sending Eviction command to %s", peer);
+                int rindex = rand()%cache->shards[i]->num_replicas;
+                int fd = shardcache_get_connection_for_peer(cache, cache->shards[i]->address[rindex]);
+                int rc = evict_from_peer(cache->shards[i]->address[rindex], (char *)cache->auth, SHC_HDR_SIGNATURE_SIP, job->key, job->klen, fd);
+                if (rc != 0)
+                    SHC_WARNING("evict_from_peer return %d for peer %s", rc, peer);
+                shardcache_release_connection_for_peer(cache, cache->shards[i]->address[rindex], fd);
             }
-
-            SHC_DEBUG2("Eviction job for key '%s' completed", keystr);
-            destroy_evictor_job(job);
-            ht_foreach_value(jobs, evict_key, &job);
         }
 
-        //if (!ht_count(jobs)) {
+        SHC_DEBUG2("Eviction job for key '%s' completed", keystr);
+        destroy_evictor_job(job);
+
+        if (!ht_count(jobs)) {
+            // if we have no more jobs to handle let's sleep a bit
             struct timeval now;
             int rc = 0;
             rc = gettimeofday(&now, NULL);
@@ -395,7 +397,7 @@ evictor(void *priv)
             } else {
                 // TODO - Error messsages
             }
-        //}
+        }
     }
     return NULL;
 }
