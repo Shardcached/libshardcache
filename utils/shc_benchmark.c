6ff9381aade409d4b8a92b5e7b4324b34e4f36b3
Andrea Guzzo <andrea.guzzo@booking.com>
2014-04-28 23:16:05 +0200

    s/%llu/%lu/g

--- utils/shc_benchmark.c
+++ utils/shc_benchmark.c
@@ -123,8 +123,6 @@ send_command(iomux_t *iomux, int fd, unsigned char *data, int *len, void *priv)
     client_ctx *ctx = (client_ctx *)priv;
     fbuf_t *output_buffer = ctx->output;
 
-    *len = 0;
-
     // don't pipeline more than 1024 requests ahead
     if (__sync_fetch_and_add(&ctx->num_requests, 0) - __sync_fetch_and_add(&ctx->num_responses, 0) < 1024)
     {
@@ -189,6 +187,8 @@ send_command(iomux_t *iomux, int fd, unsigned char *data, int *len, void *priv)
             memcpy(data, fbuf_data(output_buffer), *len);
             fbuf_remove(output_buffer, *len);
         }
+    } else {
+        *len = 0;
     }
 }
 
@@ -458,15 +458,15 @@ main (int argc, char **argv)
         if (print_stats) {
             
             printf("\033[H\033[J"
-                   "num_clients: %llu\n"
-                   "gets: %llu\n"
-                   "sets: %llu\n"
-                   "num_responses: %llu\n"
-                   "total_responses/s: %llu\n"
-                   "avg_responses/s: %llu\n"
-                   "slowest: %llu\n"
-                   "fastest: %llu\n"
-                   "stuck_clients: %llu\n",
+                   "num_clients: %lu\n"
+                   "gets: %lu\n"
+                   "sets: %lu\n"
+                   "num_responses: %lu\n"
+                   "total_responses/s: %lu\n"
+                   "avg_responses/s: %lu\n"
+                   "slowest: %lu\n"
+                   "fastest: %lu\n"
+                   "stuck_clients: %lu\n",
                    running_clients,
                    gets_total,
                    sets_total,
@@ -480,7 +480,7 @@ main (int argc, char **argv)
 
         if (stats_file) {
             char line[(10*9) + 10];
-            snprintf(line, sizeof(line), "%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu\n",
+            snprintf(line, sizeof(line), "%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu\n",
                      running_clients,
                      gets_total,
                      sets_total,
