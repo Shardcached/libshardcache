927eacf9b137c4d407b058eff4e16ac52e3e4139
xant <xant@xant.net>
2014-06-16 11:16:48 +0200

    version bump

--- src/shardcache.c
+++ src/shardcache.c
@@ -21,7 +21,7 @@
 #include "messaging.h"
 #include "shardcache_replica.h"
 
-const char *LIBSHARDCACHE_VERSION = "0.23";
+const char *LIBSHARDCACHE_VERSION = "0.24";
 
 extern int shardcache_log_initialized;
 
