7fcceb8777626667dea7d4913e7624813a4b195d
Andrea Guzzo <andrea.guzzo@booking.com>
2014-09-10 19:10:46 +0200

    version bump

--- src/shardcache.c
+++ src/shardcache.c
@@ -21,7 +21,7 @@
 #include "messaging.h"
 #include "shardcache_replica.h"
 
-const char *LIBSHARDCACHE_VERSION = "0.29";
+const char *LIBSHARDCACHE_VERSION = "0.30";
 
 extern int shardcache_log_initialized;
 
