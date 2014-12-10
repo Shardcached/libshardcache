81a6e64eccea9b4cec26a5b81a0ca5bc365133a0
Damien Krotkine <dams@zarb.org>
2014-12-10 12:46:34 +0100

    use a Hash instead of a HashRef internally

--- perl/p5-Shardcache-Client-Fast/lib/Shardcache/Client/Fast/MultifResultParser.pm
+++ perl/p5-Shardcache-Client-Fast/lib/Shardcache/Client/Fast/MultifResultParser.pm
@@ -18,32 +18,32 @@ sub process_data {
 
     $self->{_accumulator} .= $data;
 
-    my $keys_processed;
+    my %keys_processed;
 
     while (1) {
         if ($self->{_pending}) {
 
             # not enough data: bail out
             length($self->{_accumulator}) >= $self->{_pending}->{size}
-              or return $keys_processed;
+              or return \%keys_processed;
 
             # enough data: delete _pending and create result
             my $pending = delete $self->{_pending};
             my $key = $pending->{key};
             my $size = $pending->{size};
-            ($keys_processed->{$key}, $self->{_accumulator}) = unpack("a${size}a*", $self->{_accumulator});
+            ($keys_processed{$key}, $self->{_accumulator}) = unpack("a${size}a*", $self->{_accumulator});
 
         } else {
 
             # nothing pending. if not enough data, bail out
             length($self->{_accumulator}) >= 8
-              or return $keys_processed;
+              or return \%keys_processed;
 
             # enough data for a new result header
             (my $index, my $size, $self->{_accumulator}) = unpack("NNa*", $self->{_accumulator});
             my $key = $self->{keys}->[$index];
             if (length($self->{_accumulator}) >= $size) {
-                ($keys_processed->{$key}, $self->{_accumulator}) = unpack("a${size}a*", $self->{_accumulator});
+                ($keys_processed{$key}, $self->{_accumulator}) = unpack("a${size}a*", $self->{_accumulator});
             } else {
                 $self->{_pending} = { size => $size, key => $key };
             }
