ce6c17b01cf5e72231edaff76654ada9d9752173
xant <xant@xant.net>
2014-12-11 06:19:25 +0100

    next release will be version 1.0

--- src/shardcache.c
+++ src/shardcache.c
@@ -21,7 +21,7 @@
 #include "messaging.h"
 #include "shardcache_replica.h"
 
-const char *LIBSHARDCACHE_VERSION = "0.32";
+const char *LIBSHARDCACHE_VERSION = "1.0";
 
 extern int shardcache_log_initialized;
 extern unsigned int shardcache_loglevel;
