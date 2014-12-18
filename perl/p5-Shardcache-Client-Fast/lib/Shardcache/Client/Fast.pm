eb787968ea5b3ec3d96461b3fe13ec75248c230d
xant <xant@xant.net>
2014-12-18 13:14:19 +0100

    version bump + Changes

--- perl/p5-Shardcache-Client-Fast/lib/Shardcache/Client/Fast.pm
+++ perl/p5-Shardcache-Client-Fast/lib/Shardcache/Client/Fast.pm
@@ -38,7 +38,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT;
 
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
